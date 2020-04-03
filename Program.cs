28db0dc7bbc5b10f1256debb01e9bc8ee620426d
Mikkel Lauritzen <mikkel.lauritzen@youlend.com>
2020-04-03 11:00:34 +0100

    unit1  full solution

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+﻿using Akka.Actor;
+
+namespace WinTail
+{
+    #region Program
+    class Program
+    {
+        public static ActorSystem MyActorSystem;
+
+        static void Main(string[] args)
+        {
+            // initialize MyActorSystem
+            // YOU NEED TO FILL IN HERE
+            MyActorSystem = ActorSystem.Create("MyActorSystem");
+
+            // time to make your first actors!
+            //YOU NEED TO FILL IN HERE
+            // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
+            // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
+
+            var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()), "consoleWriterActor");
+
+            // make tailCoordinatorActor
+            Props tailCoordinatorProps = Props.Create(() => new TailCoordinatorActor());
+            IActorRef tailCoordinatorActor = MyActorSystem.ActorOf(tailCoordinatorProps, "tailCoordinatorActor");
+
+            // pass tailCoordinatorActor to fileValidatorActorProps (just adding one extra arg)
+            Props fileValidatorActorProps = Props.Create(() => new FileValidatorActor(consoleWriterActor));
+            IActorRef validationActor = MyActorSystem.ActorOf(fileValidatorActorProps, "validationActor");
+
+            //Props validationActorProps = Props.Create(() => new ValidationActor(consoleWriterActor));
+            // IActorRef validationActor = MyActorSystem.ActorOf(validationActorProps, "validationActor");
+            IActorRef consoleReaderActor = MyActorSystem.ActorOf(Props.Create<ConsoleReaderActor>(), "consoleReaderActor");
+
+
+
+            // tell console reader to begin
+            //YOU NEED TO FILL IN HERE
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
+            // blocks the main thread from exiting until the actor system is shut down
+            MyActorSystem.WhenTerminated.Wait();
+        }
+
+    }
+    #endregion
+}
